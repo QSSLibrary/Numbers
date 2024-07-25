6cf850f1e77aabf2e6a5080cdcfbcd6276195b72
RamtinKosari <ramtinkosari@gmail.com>
2024-07-26 00:48:00 +0330

    [#REFACTOR] Add Units Source Files

---
+++ Units.hpp
@@ -0,0 +1,29 @@
+# ifndef QSSL_UNITS
+    /**
+     * @file Units.hpp
+     * @author Ramtin Kosari (ramtinkosari@gmail.com)
+     * @brief QSS Open Source Astronomy and Astrophysics Library's Units Processing Core
+     * @date 2024-07-24
+     */
+    # define QSSL_UNITS
+    // /**
+    //  * @brief Class Definition to Set Formats of Units
+    //  */
+    // class Units {
+    //     private:
+    //         /**
+    //          * @brief Unit of Number
+    //          * @note if No Unit Assigned, It Will Get Number Unit (Unit)
+    //          */
+    //         int _unit;
+    //     public:
+    //         Units() noexcept(true) {
+    //             _unit = Unit;
+    //         }
+    //         /**
+    //          * @brief Method to Get Unit of Number
+    //          * @return int 
+    //          */
+    //         int unit() noexcept;
+    // };//__attribute__((aligned(GET_CACHE_LINE_SIZE() / 8)));
+# endif // QSSL_UNITS
